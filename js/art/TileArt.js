cc15367737dfa977653e5dd6595ee64c182dcd23
stay86625-ui <stay86625@gmail.com>
2026-06-29 00:45:10 +0800

    fix: match new texture keys

--- js/art/TileArt.js
+++ js/art/TileArt.js
@@ -1,6 +1,5 @@
 window.Game = window.Game || {};
 
-// TileArt：直接從載入的 CC0 素材取 texture
 Game.TileArt = (function () {
     function get(tileType) {
         const T = Game.SpriteLoader.textures;
@@ -8,13 +7,13 @@ Game.TileArt = (function () {
             case 'GRASS':         return T.GRASS;
             case 'GRASS_DARK':    return T.GRASS_DARK;
             case 'DIRT':          return T.DIRT;
-            case 'STONE':         return T.STONE_FLOOR;
+            case 'STONE':         return T.STONE;
             case 'WATER':         return T.WATER;
-            case 'WATER_SHALLOW': return T.WATER2;
+            case 'WATER_SHALLOW': return T.WATER_SHALLOW;
             case 'SAND':          return T.SAND;
             case 'ICE':           return T.ICE;
             case 'LAVA':          return T.LAVA;
-            case 'VOID':          return T.CAVE_FLOOR;
+            case 'VOID':          return T.VOID;
             default:              return T.DIRT;
         }
     }
